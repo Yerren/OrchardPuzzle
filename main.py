21f4bfdbc7b14eaf539310df0de52e2df5ca794e
DESKTOP-WORKSTATION <yerren@renerica.com>
2024-03-24 14:34:45 +1300

    Added basic recursive search with early stopping

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from collections.abc import Iterable
 from collections.abc import Collection
 import time
 
-GRID_SIZE = 8
-
 
 def check_overlap(config_1: np.ndarray, config_2: np.ndarray) -> bool:
     """
@@ -95,14 +93,11 @@ def apply_all_transformations(configs_in: Iterable[np.ndarray]) -> np.ndarray:
     return np.array(rotated_configs)
 
 
-def plot_solution(config_1: np.ndarray, config_2: np.ndarray, config_3: np.ndarray, config_4: np.ndarray) -> None:
+def plot_solution(configs_in: Iterable[np.ndarray]) -> None:
     """
-    Plots the four provided configurations.
+    Plots up to four provided configurations in different colours.
     
-    :param config_1: The first configuration to plot.
-    :param config_2: The second configuration to plot.
-    :param config_3: The third configuration to plot.
-    :param config_4: The forth configuration to plot.
+    :param configs_in: The configuration(s) to plot.
     """
     fig, ax = plt.subplots()
 
@@ -114,26 +109,24 @@ def plot_solution(config_1: np.ndarray, config_2: np.ndarray, config_3: np.ndarr
     cmap_house = np.array([[0, 0, 0, 0], [150 / 256, 150 / 256, 150 / 256, 1]])
 
     # Create new colormaps
-    cmap_1 = ListedColormap(cmap_1)
-    cmap_2 = ListedColormap(cmap_2)
-    cmap_3 = ListedColormap(cmap_3)
-    cmap_4 = ListedColormap(cmap_4)
+    cmap_list = [ListedColormap(cmap_1), ListedColormap(cmap_2), ListedColormap(cmap_3), ListedColormap(cmap_4)]
+
     cmap_house = ListedColormap(cmap_house)
 
-    # Overlay the four arrays on top of each other in different colors
-    ax.imshow(config_1, cmap=cmap_1)
-    ax.imshow(config_2, cmap=cmap_2)
-    ax.imshow(config_3, cmap=cmap_3)
-    ax.imshow(config_4, cmap=cmap_4)
+    # Overlay the arrays on top of each other in different colors, with the house always included at the end.
+    for config, cmap in zip(configs_in, cmap_list):
+        ax.imshow(config, cmap=cmap)
     ax.imshow(house_config, cmap=cmap_house)
 
+    plt.show()
 
-def check_combs_iterative_brute_force(configs_in: Collection[np.ndarray]) -> bool:
+
+def check_combs_iterative_brute_force(configs_in: Collection[np.ndarray]) -> tuple | None:
     """
     Iteratively checks all possible configurations using brute force. Very inefficient, but simple.
 
     :param configs_in: All valid configurations to check (including transformations).
-    :return: Whether a valid solution was found.
+    :return: A tuple containing the solution configurations, or None, if no solution is found.
     """
     print(f"{len(configs_in) ** 4} combinations to check")
 
@@ -143,41 +136,74 @@ def check_combs_iterative_brute_force(configs_in: Collection[np.ndarray]) -> boo
                 for choice_4 in configs_in:
                     output = house_config + choice_1 + choice_2 + choice_3 + choice_4
                     if output.sum() == 44:
-                        plot_solution(choice_1, choice_2, choice_3, choice_4)
-                        return True
+                        return tuple([choice_1, choice_2, choice_3, choice_4])
 
-    return False
-
-
-configs = np.zeros((5, 8, 8), dtype='bool')
-config_indices = np.array([[[7, 0], [5, 1], [0, 2], [3, 2], [5, 2], [6, 2], [1, 3], [5, 4], [4, 6], [5, 7]],
-                           [[0, 0], [0, 3], [0, 5], [0, 6], [1, 4], [2, 2], [3, 0], [4, 1], [5, 0], [6, 0]],
-                           [[0, 1], [1, 3], [2, 5], [3, 1], [3, 2], [3, 4], [3, 7], [5, 1], [6, 1], [7, 0]],
-                           [[0, 3], [2, 2], [2, 4], [3, 3], [4, 1], [4, 2], [4, 4], [4, 5], [6, 0], [6, 6]],
-                           [[0, 0], [3, 0], [3, 3], [4, 2], [5, 0], [5, 1], [5, 4], [5, 5], [6, 0], [6, 6]]])
-
-house_config = np.zeros((8, 8), dtype='bool')
-house_config[6, 3] = True
-house_config[6, 4] = True
-house_config[7, 3] = True
-house_config[7, 4] = True
+    return None
 
 
-for i, indices in enumerate(config_indices):
-    configs[i][indices[:, 0], indices[:, 1]] = True
+def check_combs_recursive_brute_force(configs_in: Collection[np.ndarray], current_config: np.ndarray,
+                                      num_configs: int = 0, selected_configs: tuple = ()) -> tuple | None:
+    """
+    Recursively checks all possible configurations using brute force. Inefficient, but simple.
 
-final_configs = apply_all_transformations(configs)
-print(f"{len(final_configs)} configs")
+    :param configs_in: All valid configurations to check (including transformations).
+    :param current_config: The current configuration, consisting of all the selected configs added together.
+    :param num_configs: The number of configurations already selected.
+    :param selected_configs: A tuple of the configurations selected so far.
+    :return: A tuple containing the solution configurations, or None, if no solution is found.
+    """
+
+    # Don't go deeper in the search if this configuration already is invalid
+    if current_config.sum() != 4 + 10 * num_configs:
+        return None
+
+    # If we are at the final layer of the search tree, return whether the solution is valid.
+    if num_configs == 4 and current_config.sum() == 44:
+        return selected_configs
+
+    for config in configs_in:
+        result = check_combs_recursive_brute_force(configs_in, current_config + config, num_configs + 1,
+                                                   selected_configs + tuple([config]))
+        if result is not None:
+            return result
+
+    return None
 
 
-# Fully naive brute force search
-start_time = time.time()
-solution_found = check_combs_iterative_brute_force(final_configs)
-finish_time = time.time()
+if __name__ == "__main__":
+    GRID_SIZE = 8
 
-elapsed_time = finish_time - start_time
-if solution_found:
-    print(f"Found a solution in {elapsed_time} seconds.")
-    plt.show()
-else:
-    print(f"No solution found. Took {elapsed_time} seconds.")
+    """
+    Set up the initial valid configurations manually, using the logic described in the readme.
+    """
+    configs = np.zeros((5, 8, 8), dtype='bool')
+    config_indices = np.array([[[7, 0], [5, 1], [0, 2], [3, 2], [5, 2], [6, 2], [1, 3], [5, 4], [4, 6], [5, 7]],
+                               [[0, 0], [0, 3], [0, 5], [0, 6], [1, 4], [2, 2], [3, 0], [4, 1], [5, 0], [6, 0]],
+                               [[0, 1], [1, 3], [2, 5], [3, 1], [3, 2], [3, 4], [3, 7], [5, 1], [6, 1], [7, 0]],
+                               [[0, 3], [2, 2], [2, 4], [3, 3], [4, 1], [4, 2], [4, 4], [4, 5], [6, 0], [6, 6]],
+                               [[0, 0], [3, 0], [3, 3], [4, 2], [5, 0], [5, 1], [5, 4], [5, 5], [6, 0], [6, 6]]])
+
+    house_config = np.zeros((8, 8), dtype='bool')
+    house_config[6, 3] = True
+    house_config[6, 4] = True
+    house_config[7, 3] = True
+    house_config[7, 4] = True
+
+    for i, indices in enumerate(config_indices):
+        configs[i][indices[:, 0], indices[:, 1]] = True
+
+    final_configs = apply_all_transformations(configs)
+    print(f"{len(final_configs)} possible configurations (including transformations).")
+
+    # Fully naive brute force search
+    start_time = time.time()
+    # solution_configs = check_combs_iterative_brute_force(final_configs)
+    solution_configs = check_combs_recursive_brute_force(final_configs, house_config)
+    finish_time = time.time()
+
+    elapsed_time = finish_time - start_time
+    if solution_configs is not None:
+        print(f"Found a solution in {elapsed_time} seconds.")
+        plot_solution(solution_configs)
+    else:
+        print(f"No solution found. Took {elapsed_time} seconds.")
